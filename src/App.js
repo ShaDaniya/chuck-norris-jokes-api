a388054909c6955bef02468923abef083633a811
Daniya Shagieva <daniyashagieva@gmail.com>
2023-04-11 20:01:46 +0300

    chuck norris jokes generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import chuckNorris from './assets/chuck-norris.jpeg'
+
+const API_URL = "https://api.chucknorris.io/jokes/random"
 
 function App() {
+  const [joke, setJoke] = useState('')
+
+  const generateJoke = () => {
+    fetch(API_URL)
+      .then (res => res.json())
+      .then (data => setJoke(data.value))
+  }
+
+  useEffect(() => {
+    generateJoke()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <h2>Chuck Norris Jokes Generator</h2>
+      <div>
+        <img src={chuckNorris} alt="" />
+      </div>
+      <p>{joke}</p>
+      <button onClick={generateJoke}>Get new joke 😺  </button>
     </div>
   );
 }
